b42a3ab0892448031ad923cde7201e75949d8b3d
Liora-1725 <gamingpasion818@gmail.com>
2026-04-25 20:23:59 +0200

    Update StatusEffectInstanceMixin.java

--- src/main/java/me/alpha432/oyvey/mixin/entity/StatusEffectInstanceMixin.java
+++ src/main/java/me/alpha432/oyvey/mixin/entity/StatusEffectInstanceMixin.java
@@ -1,16 +1,17 @@
-package meteordevelopment.meteorclient.mixin.entity;
+package oyvey.mixin.entity;
 
-import meteordevelopment.meteorclient.mixininterface.IStatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StatusEffectInstance.class)
-public abstract class StatusEffectInstanceMixin implements IStatusEffectInstance {
+public class StatusEffectInstanceMixin {
+    // В 1.21 можно просто через Shadow менять поле, если миксин в том же пакете
     @Shadow private int duration;
 
-    @Override
-    public void meteor$setDuration(int duration) {
-        this.duration = duration;
-    }
+    // Этот метод можно вызвать из модуля, если сделать интерфейс, 
+    // но для Potion режима в Oyvey проще просто перенакладывать эффект (как в коде выше)
 }
