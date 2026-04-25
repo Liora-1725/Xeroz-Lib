d32c2b63eea8ebae7668ab3be50eaf7795e06030
Liora-1725 <gamingpasion818@gmail.com>
2026-04-25 20:13:52 +0200

    Create IKeyBinding.java

---
+++ src/main/java/me/alpha432/oyvey/mixin/input/IKeyBinding.java
@@ -0,0 +1,12 @@
+package me.alpha432.oyvey.mixin.input;
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyBinding.class)
+public interface IKeyBinding {
+    @Accessor("boundKey")
+    InputUtil.Key getBoundKey();
+}
