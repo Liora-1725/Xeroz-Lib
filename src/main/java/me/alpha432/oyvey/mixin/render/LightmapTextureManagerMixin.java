dc37d0e6810f866da9db2cfa26a6d131dfea738f
Liora-1725 <gamingpasion818@gmail.com>
2026-04-25 20:22:22 +0200

    Update LightmapTextureManagerMixin.java

--- src/main/java/me/alpha432/oyvey/mixin/render/LightmapTextureManagerMixin.java
+++ src/main/java/me/alpha432/oyvey/mixin/render/LightmapTextureManagerMixin.java
@@ -1,7 +1,6 @@
-package meteordevelopment.meteorclient.mixin.render;
+package oyvey.mixin.render;
 
-import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.player.Fullbright;
+import oyvey.features.modules.player.Fullbright;
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,11 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class LightmapTextureManagerMixin {
     @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
     private static void onGetBrightness(CallbackInfoReturnable<Float> info) {
-        Fullbright fullbright = Modules.get().get(Fullbright.class);
-        
-        // Режим Gamma: заставляем игру думать, что яркость всегда на максимуме
-        if (fullbright != null && fullbright.isActive() && fullbright.getGamma()) {
-            info.setReturnValue(1000f); 
+        if (Fullbright.INSTANCE != null && Fullbright.INSTANCE.isEnabled() && Fullbright.INSTANCE.mode.getValue() == Fullbright.Mode.Gamma) {
+            info.setReturnValue(1000f);
         }
     }
 }
